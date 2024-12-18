daa5621232bbb6db01700dab1d78b72fa415e3bc
Nyandiekahh <166758870+Nyandiekahh@users.noreply.github.com>
2024-12-18 11:02:43 +0300

    Update Navbar.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -222,7 +222,7 @@ const Navbar = () => {
       <NavContent>
         <Logo whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
           <div className="logo-icon">BGS</div>
-          <span>Beyond Gravity</span>
+          <span>Bold and Brilliant</span>
         </Logo>
 
         <NavLinks>
